92b9f1dc5330852b404e2a473d487f6bb03460b3
CapOfCats <daniil.daragan@mail.ru>
2021-10-30 19:51:06 +0300

    Added raw migration

---
+++ ChatService/Migrations/20211030162423_InitialCreate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace СhatService.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
